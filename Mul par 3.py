6afcadee8c6381ce1b40495040074348fcc9acc0
juniorcortex7423 <tchakalamatinou1@gmail.com>
2025-04-19 04:20:25 -0400

    first commit

---
+++ Mul par 3.py
@@ -0,0 +1,9 @@
+# Table de multiplication par 3
+
+multiplicateur = 3
+
+print(f"Table de multiplication par {multiplicateur} :\n")
+
+for i in range(1, 11):
+    resultat = multiplicateur * i
+    print(f"{multiplicateur} x {i:2} = {resultat}")
